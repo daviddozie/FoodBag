44bde6a925a645960493f996e49673607fd391c0
David Mgbede <davidmgbede75@gmail.com>
2023-10-06 12:51:12 +0100

    updates

--- js/index.js
+++ js/index.js
@@ -14,3 +14,18 @@ for (i = 1; i <= 4; i++) {
     })
 
 }
+
+const text = document.querySelector(".shaking-text");
+
+function startShake() {
+    text.style.animation = "shake 1s ease-in-out";
+    setTimeout(() => {
+        text.style.animation = "none"; // Stop the animation
+        setTimeout(() => {
+            text.style.animation = "shake 1s ease-in-out"; // Resume animation
+            setTimeout(startShake, 4000); // Wait for 20 seconds and repeat
+        }, 1000);
+    }, 1000);
+}
+
+startShake(); // Start the animation
